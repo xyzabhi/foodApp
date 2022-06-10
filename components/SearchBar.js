9950337be22bf4b10d7ce374ac864d1361a724b4
Abhinav Kumar <abhinavkumar8083@gmail.com>
2022-06-10 14:59:20 +0530

    Search Bar completed in Food APP

---
+++ components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, TextInput, Text, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    flexDirection: "row",
+    backgroundColor: "#f0eeee",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 32,
+    alignSelf: "center",
+    marginHorizontal: 10,
+  },
+});
+
+export default SearchBar;
